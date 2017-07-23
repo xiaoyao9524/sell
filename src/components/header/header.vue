4b1095b9aebf98b4a75983a88bdc5e788df1286b
xiaoyao9524 <xiaoyao9524@163.com>
2017-07-23 22:08:45 +0800

    2017-07-23

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,30 +17,76 @@
           <span class="text">{{ seller.supports[0].description }}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{ seller.supports.length }}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width="100%" height="200%" />
     </div>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <line-title :message="'优惠信息'"></line-title>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="item in seller.supports" :key="item.id">
+                <span class="icon" :class="classMap[item.type]"></span>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <line-title :message="'商家信息'"></line-title>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close">
+          <i class="icon-close"  @click="hiddenDetail"></i>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import star from '../star/star';
+  import lineTitle from '../line-title/line-title';
+
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    data () {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true;
+      },
+      hiddenDetail: function () {
+        this.detailShow = false;
+      }
+    },
     created () {
         this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+    },
+    components: {
+      star,
+      lineTitle
     }
   };
 </script>
@@ -163,6 +209,84 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+    .detail
+      position: fixed
+      left: 0
+      top: 0
+      z-index: 100
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7,17,27,0.8)
+      backdrop-filter: blur(10px)
+      &.fade-enter-active, &.fade-leave-active
+        transition: opacity .5s
+      &.fade-enter
+        opacity: 0
+      &.fade-enter-to
+        opacity: 1
+        background: rgba(7,17,27,0.8)
+      &.fade-leave-to
+        opacity: 0
+      .detail-wrapper
+        width: 100%
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+          .star-wrapper
+            margin-top: 16px
+            padding: 2px 0
+            text-align: center
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  @include bg-image('decrease_2')
+                &.discount
+                  @include bg-image('discount_2')
+                &.guarantee
+                  @include bg-image('guarantee_2')
+                &.invoice
+                  @include bg-image('invoice_2')
+                &.special
+                  @include bg-image('special_2')
+              .text
+                line-height: 16px
+                font-size: 12px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 
 
 </style>
---
+++ src/components/line-title/line-title.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="line-title">
+    <div class="line"></div>
+    <div class="text">{{message}}</div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      message: {
+        Type: String
+      }
+    }
+  };
+</script>
+
+<style lang="sass" rel="stylesheet/sass">
+  .line-title
+    display: flex
+    width: 80%
+    margin: 28px auto 24px auto
+    .line
+      flex: 1
+      position: relative
+      top: -6px
+      border-bottom: 1px solid rgba(255,255,255,0.2)
+    .text
+      padding: 0 12px
+      font-weight: 700
+      font-size: 14px
+</style>
